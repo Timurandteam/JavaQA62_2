b531c4f9dd377ab1fb871342c8052fbde9a1e74e
Tima test <timmy12122022@gmail.com>
2023-06-15 14:47:46 +0700

    Второе задание

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 100;
+        int s = 12000;
+        int i = s / 100;
+
+        System.out.println("Счёт клиента :" + x);
+        System.out.println("Сумма пополнения :" + s);
+        if (s > 1050) {
+            System.out.println("Сумма бонусов :" + i);
+        }
+        else {
+            System.out.println("Сумма бонусов :" + 0);
+        }
+        int a = x + s + i;
+        if (s > 1050) {
+            System.out.println("Итого :" + a);
+        }
+        else {
+            System.out.println("Итого :" + (x + s));
+        }
+    }
+}
