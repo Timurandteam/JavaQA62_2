7b345345b5d218e7ea8ef6d3b43128e357cf1322
Tima test <timmy12122022@gmail.com>
2023-06-19 17:43:31 +0700

    zadanie2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 100;
-        int s = 12000;
-        int i = s / 100;
+        int balance = 100;
+        int score = 10_000;
+        int i = score / 100;
 
-        System.out.println("Счёт клиента :" + x);
-        System.out.println("Сумма пополнения :" + s);
-        if (s > 1050) {
+        System.out.println("Счёт клиента :" + balance);
+        System.out.println("Сумма пополнения :" + score);
+        if (score > 1050) {
             System.out.println("Сумма бонусов :" + i);
         } else {
             System.out.println("Сумма бонусов :" + 0);
         }
-        int a = x + s + i;
-        if (s > 1050) {
+        int a = balance + score + i;
+        if (score > 1050) {
             System.out.println("Итого :" + a);
         } else {
-            System.out.println("Итого :" + (x + s));
+            System.out.println("Итого :" + (balance + score));
         }
     }
 }
